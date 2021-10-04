b668f1d180addd0be063612a07fcf8e0d00d7166
Alhif <72171338+alhifikhsan@users.noreply.github.com>
2021-10-04 20:17:21 +0700

    go[menambah function encode dan decode base64]

--- go/main.go
+++ go/main.go
@@ -6,4 +6,8 @@ func main()  {
 	fmt.Println("FOSTI UMS")
 	hasil := linearSearch([]int{1, 5, 4, 3, 9, 3, 5}, 90)
 	fmt.Println(hasil)
+
+	//menambah function untuk encode dan decode base64
+	encode()
+	decode()
 }
